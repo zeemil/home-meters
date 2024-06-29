94e89c1dba952831ee97c784c0a0225c334f611b
Emilien Fritschy <ze_emil@bluewin.ch>
2024-06-29 17:17:06 +0200

    Add Meter Model factory and seeder
    Add relation with Location

---
+++ app/Models/Meter.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Meter extends Model
+{
+    use HasFactory;
+
+    public function location(): BelongsTo
+    {
+        return $this->belongsTo(Location::class);
+    }
+}
---
+++ database/factories/MeterFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Location;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Meter>
+ */
+class MeterFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'ean_code' => fake()->randomNumber(),
+            'label' => fake()->text(),
+        ];
+    }
+}
