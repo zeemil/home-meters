b22a024c18c63d4046885940341bcf8faf6f52af
Emilien Fritschy <ze_emil@bluewin.ch>
2024-07-21 16:45:08 +0200

    Home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,28 @@
 @section('title', ' > '.__('Home'))
 
 
+
 @section('content')
-   <ul class="bg-gray-50">
-    <li>Meters</li>
-    <li>Car</li>
-   </ul>
+
+<div class="grid grid-rows-2 grid-flow-col gap-4">
+   <h1>{{ __('What do you want to record ?')}}</h1>
+   <div class="grid grid-cols-1 gap-4">
+        @foreach($links as $title => $url)
+        <div>
+            <a href={{$url}} class="
+            rounded-lg 
+            px-3 py-2
+            text-slate-700
+            font-medium
+            bg-slate-50
+            hover:bg-slate-100
+            hover:text-slate-900">
+            {{$title}}
+            </a>
+        </div>
+        @endforeach
+    </div>
+     
+   
+</div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,13 @@
 use App\Http\Controllers\LocationController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function(){ return view('Welcome'); });
+Route::get('/', function(){ 
+    $links = [
+        'Meters' => route('locations.index'),
+        'Cars' => '/',
+    ];
+    return view('Welcome', compact('links'));
+ });
 Route::resource('locations', LocationController::class)->only([
     'index', 'create'
 ]);
